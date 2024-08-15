177f8342e724af84b45fe5900c889fb12ad93c4c
Oluwagbenge Adedire <adediredayo@gmail>
2024-08-15 04:28:51 +0100

    fix error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // This ensures that Rollup can resolve the 'react-icons/fa' module
+      external: ["react-icons/fa"],
+    },
+  },
 });
