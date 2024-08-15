aa28f647a22d37141f6a36e2d536ee6298c63371
Oluwagbenge Adedire <adediredayo@gmail>
2024-08-15 10:08:57 +0100

    fixed react-icon error

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react-icons/*", // This will handle all imports from react-icons
-      ],
-    },
-  },
 });
